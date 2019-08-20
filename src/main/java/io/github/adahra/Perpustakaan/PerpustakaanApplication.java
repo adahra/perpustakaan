7b771fca1f6eb9693aabf3317e5defff0c5ba809
Adnanto Ahmad Romadhon <adnanto.ahmad@sebangsa.com>
2019-08-20 11:40:32 +0700

    Change database name

--- src/main/java/io/github/adahra/Perpustakaan/PerpustakaanApplication.java
+++ src/main/java/io/github/adahra/Perpustakaan/PerpustakaanApplication.java
@@ -8,8 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class PerpustakaanApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PerpustakaanApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PerpustakaanApplication.class, args);
+    }
 }
